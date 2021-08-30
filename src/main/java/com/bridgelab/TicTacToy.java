1ac31a95827249442188afd3b1d31d4f7aa05990
ToshitaC <toshitachaudhari17@gmail.com>
2021-08-30 09:56:44 +0530

    Added:Assigned Value For TicTacToe Game

--- src/main/java/com/bridgelab/TicTacToy.java
+++ src/main/java/com/bridgelab/TicTacToy.java
@@ -1,7 +1,15 @@
 package com.bridgelab;
 
 public class TicTacToy {
-    public static void main(String[] args) {
-        System.out.println("!!Welcome to TicTacToy Game!!");
+ static char[] board =new char[10];
+ public static void TicTacToyInitialization(){;
+         for( int i=1;i<board.length;i++){
+        board[i]=' ';
     }
 }
+    public static void main(String[] args) {
+        TicTacToyInitialization();
+    }
+}
+
+
