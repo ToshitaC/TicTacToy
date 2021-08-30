c57a35b45eaad03e1d8148caab77ef224fdebaee
ToshitaC <toshitachaudhari17@gmail.com>
2021-08-30 10:09:17 +0530

    Added:User Choose X or O

--- src/main/java/com/bridgelab/TicTacToy.java
+++ src/main/java/com/bridgelab/TicTacToy.java
@@ -1,15 +1,34 @@
 package com.bridgelab;
 
+import java.util.Scanner;
+
 public class TicTacToy {
- static char[] board =new char[10];
- public static void TicTacToyInitialization(){;
-         for( int i=1;i<board.length;i++){
-        board[i]=' ';
+    static char[] board = new char[10];
+
+    public static void ticTakToeInitialization() {
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
     }
-}
+
+    static void playerChose() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("User choose 0 or X");
+        char playerChoose = scanner.next().charAt(0);
+        if (playerChoose == 'X' || playerChoose == 'X') {
+            System.out.println("User Chosen X");
+            System.out.println("Computer Choice O");
+        } else {
+            System.out.println("User Chosen O");
+            System.out.println("computer Choice X ");
+        }
+
+
+    }
+
     public static void main(String[] args) {
-        TicTacToyInitialization();
+        System.out.println("!!Welcome to TicTakToe Game!!");
+        ticTakToeInitialization();
+        playerChose();
     }
 }
-
-
